3064afd6ec0917bd2984b67764db6d80e97ba92f
jawlecks <alex@alexjolly.me>
2018-04-20 22:18:01 -0400

    Added average SR for each team

--- src/inject.js
+++ src/inject.js
@@ -13,20 +13,46 @@ function getBattleTags(team) {
 }
 
 function displayStats(battleTags) {
+    let team1Average = $("<p>Average SR: </p>");
+    let team2Average = $("<p>Average SR: </p>");
+    let team1TotalRank = 0, team1TotalRanked = 0;
+    let team2TotalRank = 0, team2TotalRanked = 0;
+
     battleTags.map(function(i, battleTag) {
         let battleTagNoNumbers = battleTag.split('-')[0];
         let player = $(".match-detail__players .no-margin:contains('" + battleTagNoNumbers + "')");
         addBorder(player);
 
         getStats(battleTag, function(rank, rankImg, times) {
-            displayRank(player, rank, rankImg);
-            displayTimes(player, times);
+            if (rank) {
+                if (isTeam1(player)) {
+                    team1TotalRank += rank;
+                    team1TotalRanked += 1;
+                } else {
+                    team2TotalRank += rank;
+                    team2TotalRanked += 1;
+                }
+                displayRank(player, rank, rankImg);
+            }
+            if (times) {
+                displayTimes(player, times);
+            }
+            if (team1TotalRanked > 0) {
+                updateAverageSR(team1Average, team1TotalRank / team1TotalRanked);
+            } else if (team2TotalRanked > 0) {
+                updateAverageSR(team2Average, team2TotalRank / team2TotalRanked);
+            }
         });
     });
+
+    $('.match-detail__players_team1 .player-stats').first().before(team1Average);
+    $('.match-detail__players_team2 .player-stats').first().before(team2Average);
+    addBorder(team1Average);
+    addBorder(team2Average);
 }
 
 function addBorder(player) {
-    player.wrap('<div></div>');
+    player.wrap('<div class="player-stats"></div>');
     if (!player.parent().is(':last-child')) {
         let div = $('<div></div>');
         div.css({
@@ -55,47 +81,43 @@ function getStats(battleTag, callback) {
 }
 
 function displayRank(player, rank, rankImg) {
-    if (rank) {
-        let span = player.find('span');
-        if (isTeam1(player)) {
-            span.text(span.text() + ' | ' + rank);
-        } else {
-            span.text(rank + ' | ' + span.text());
-        }
-
-        let img = player.find('img');
-        img.attr('src', rankImg);
-        img.width(32);
-        img.height(32);
+    let span = player.find('span');
+    if (isTeam1(player)) {
+        span.text(span.text() + ' | ' + rank);
+    } else {
+        span.text(rank + ' | ' + span.text());
     }
+
+    let img = player.find('img');
+    img.attr('src', rankImg);
+    img.width(32);
+    img.height(32);
 }
 
 function displayTimes(player, times) {
-    if (times) {
-        let timesArray = [];
-        for (key in times) {
-            if (times[key] > 0) {
-                timesArray.push([key == 'soldier76' ? 'soldier-76' : key, times[key]]);
-            }
+    let timesArray = [];
+    for (key in times) {
+        if (times[key] > 0) {
+            timesArray.push([key == 'soldier76' ? 'soldier-76' : key, times[key]]);
         }
+    }
 
-        timesArray.sort(function(pair1, pair2) { return pair2[1] - pair1[1]; });
-        if (timesArray.length > 5) {
-            timesArray = timesArray.slice(0, 5);
-        }
+    timesArray.sort(function(pair1, pair2) { return pair2[1] - pair1[1]; });
+    if (timesArray.length > 5) {
+        timesArray = timesArray.slice(0, 5);
+    }
 
-        if (isTeam1(player)) {
-            timesArray.forEach(function(pair) {
-                player.after(getImgForHero(pair[0]));
-            });
-        } else {
-            let current = player;
-            timesArray.forEach(function(pair) {
-                let img = getImgForHero(pair[0]);
-                current.after(img);
-                current = img;
-            });
-        }
+    if (isTeam1(player)) {
+        timesArray.forEach(function(pair) {
+            player.after(getImgForHero(pair[0]));
+        });
+    } else {
+        let current = player;
+        timesArray.forEach(function(pair) {
+            let img = getImgForHero(pair[0]);
+            current.after(img);
+            current = img;
+        });
     }
 }
 
@@ -113,4 +135,12 @@ function getImgForHero(hero) {
         'border-radius': '2px'
     });
     return img;
+}
+
+function updateAverageSR(element, averageSR) {
+    let currentText = element.text();
+    if (currentText != 'Average SR: ') {
+        currentText = currentText.slice(0, 12);
+    }
+    element.text(currentText + Math.floor(averageSR));
 }
